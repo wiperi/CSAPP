8266fa11702879f6aaa4f4510a912b1656b5bbed
Magician <solidtreepassing@gmail.com>
2024-03-28 00:27:38 +1100

    finish lab02

--- MIPS_demo/if.c
+++ MIPS_demo/if.c
@@ -11,11 +11,24 @@ int main(void) {
 
 int simplified(int argc, char* argv[]) {
     int i, n;
-    if (i >= 0)
-        goto else1;
+    if (i >= 0) goto else1;
     n = n - i;
     goto end1;
 else1:
     n = n + i;
+end1:
+}
+
+int simplified_2() {
+    int i, n;
+
+    if (i < 0) goto then1;
+    goto else1;
+then1:
+    n = n - i;
+    goto end1;
+else1:
+    n = n + i;
+    goto end1;
 end1:
 }
