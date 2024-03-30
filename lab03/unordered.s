1164d60f9df0acf513a896166deb5899c17a6b09
Magician <solidtreepassing@gmail.com>
2024-03-30 16:58:53 +1100

    up unordered

--- lab03/unordered.s
+++ lab03/unordered.s
@@ -12,7 +12,7 @@ main:
 	#  - $t1: temporary result
 	#  - $t2: temporary result
 	#  TODO: add your registers here
-
+	#  - $t3: swapped
 scan_loop__init:
 	li	$t0, 0				# i = 0;
 scan_loop__cond:
@@ -32,9 +32,33 @@ scan_loop__body:
 scan_loop__end:
 
 	# TODO: add your code here!
+loop1_init:
+	li $t3, 0 # $t3 = swapped = 0
+	li $t0, 1 # $t0 = i = 1
+loop1_con:
+	bge $t0, ARRAY_LEN, j loop1_end # while (i < ARRAY_LEN)
+loop1_body:
+	mul $t0, 4 	      # i * 4
+	addi $t1, numbers # numbers + i * 4
+	lw $t1, ($t1) # x = numbers[i]
+
+	mul $t0, 4 	      # i * 4
+	addi $t2, numbers # numbers + i * 4
+	lw $t2, ($t2) # y = numbers[i]
+
+if1_con:
+	blt $t1, $t2, then1
+	j if1_end
+then1:
+	li $t3, 1
+if1_end:
+
+	addi $t0, $t0, 1 # i++
+	j loop1_con
+loop1_end:
 
 	li	$v0, 1				# syscall 1: print_int
-	li	$a0, 42 			#
+	move  $a0, $t3 			#
 	syscall					# printf("%d", 42)
 
 	li	$v0, 11				# syscall 11: print_char
