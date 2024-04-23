768ff3ec1867c6d89cf1ab1275c9cae1bf1c5b30
Magician <solidtreepassing@gmail.com>
2024-04-24 04:56:36 +1000

    up

--- lab07/addi.c
+++ lab07/addi.c
@@ -7,32 +7,39 @@
 
 #include "addi.h"
 
+// #include "C:\Users\15617\Documents\92_MyGithub\CSAPP\ppt_bitwise\print_bits.h"
+
 // return the encoded binary MIPS for addi $t,$s, i
 uint32_t addi(int t, int s, int i) {
 
-    assert(t > 0 && t < 32);
-    assert(s > 0 && s < 32);
+    assert(t >= 0 && t < 32);
+    assert(s >= 0 && s < 32);
     assert(i < (1 << 16));
 
     uint32_t res = 0;
 
     // build opcode
-    res = 0b001000 << 26;
+    // res = 0b001000 << 26;
+    print_bits(res, 32);
 
     // build source
     res |= s << 21;
+    // print_bits(res, 32);
 
     // build dest
     res |= t << 16;
+    // print_bits(res, 32);
 
     // build imm val
-    res |= i;
+    res |= (uint16_t) i;
+    // res |= ((((unsigned) i) << 16) >> 16);
+    // print_bits(res, 32);
 
     return res;
 }
 
 int main(int argc, char const* argv[]) {
-    addi(9, 27, 42);
+    addi(31, 31, -1);
 
     return 0;
 }
