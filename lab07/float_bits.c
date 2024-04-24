714fdb04f8f70ea3306d02c6e6d009e7f5dd9101
Magician <solidtreepassing@gmail.com>
2024-04-25 00:26:45 +1000

    up

--- lab07/float_bits.c
+++ lab07/float_bits.c
@@ -26,11 +26,11 @@ float_components_t float_bits(uint32_t f) {
     // PUT YOUR CODE HERE
     float_components_t res = {};
 
-    res.sign = f & (SIGN_MASK << SIGN_BIT);
+    res.sign = (f & (SIGN_MASK << SIGN_BIT)) >> SIGN_BIT;
 
-    res.exponent = f & (EXPONENT_MASK << EXPONENT_LOW_BIT);
+    res.exponent = (f & (EXPONENT_MASK << EXPONENT_LOW_BIT)) >> EXPONENT_LOW_BIT;
 
-    res.fraction = f & (FRACTION_MASK << FRACTION_LOW_BIT);
+    res.fraction = (f & (FRACTION_MASK << FRACTION_LOW_BIT)) >> FRACTION_LOW_BIT;
 
     return res;
 }
@@ -65,4 +65,25 @@ int is_zero(float_components_t f) {
     // PUT YOUR CODE HERE
 
     return f.exponent == 0 && f.fraction == 0;
+}
+
+int main(int argc, char *argv[]) {
+    for (int arg = 1; arg < argc; arg++) {
+        union overlay input;
+
+        input.f = atof(argv[arg]);
+        float_components_t c = float_bits(input.u);
+
+        printf("float_bits(%.9g) returned\n", input.f);
+        printf("sign=0x%x\n", c.sign);
+        printf("exponent=0x%02x\n", c.exponent);
+        printf("fraction=0x%06x\n", c.fraction);
+
+        printf("is_nan returned %d\n", is_nan(c));
+        printf("is_positive_infinity returned %d\n", is_positive_infinity(c));
+        printf("is_negative_infinity returned %d\n", is_negative_infinity(c));
+        printf("is_zero returned %d\n", is_zero(c));
+    }
+
+    return 0;
 }
