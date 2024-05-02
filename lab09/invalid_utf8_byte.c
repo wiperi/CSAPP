1eefe2121fa21ee0049985878a4e0b6820d89ca7
Magician <solidtreepassing@gmail.com>
2024-05-02 14:25:16 +1000

    up

--- lab09/invalid_utf8_byte.c
+++ lab09/invalid_utf8_byte.c
@@ -3,23 +3,24 @@
 
 // Do NOT change this function's return type or signature.
 
-#include <stdint.h>
-
 int invalid_utf8_byte(char* utf8_string) {
 
     // TODO: implement this function
 
     for (int i = 0; utf8_string[i] != '\0'; i++) {
 
-        uint8_t ch = utf8_string[i];
+        unsigned int ch = utf8_string[i];
 
         if ((ch >> 7) == 0) {
             // 0xxx = good
             continue;
+        } else if ((ch >> 6) == 0b10) {
+            // sole 10xx = not good
+            return i;
         } else {
             // 1xxx = there are followings
             int k = 7;
-            int len = 0;
+            int len = -1;
             while (k >= 0) {
                 if (((ch >> k) & 1) == 1) {
                     len++;
@@ -29,8 +30,8 @@ int invalid_utf8_byte(char* utf8_string) {
                 k--;
             }
 
-            // check successive byte
             for (int j = 0; j < len; j++) {
+                // check successive byte
                 i++;
                 ch = utf8_string[i];
                 printf("%02x\n", ch);
@@ -38,7 +39,7 @@ int invalid_utf8_byte(char* utf8_string) {
                 if ((ch >> 6) == 0b10) {
                     continue;
                 } else {
-                    return i + 1;
+                    return i;
                 }
             }
         }
