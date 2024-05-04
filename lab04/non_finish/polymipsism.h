8cd0ae6a3687e9c52389cf1bb44800e74e66cf98
Magician <solidtreepassing@gmail.com>
2024-05-04 15:55:24 +1000

    extract DPST1092

--- lab04/non_finish/polymipsism.h
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-typedef void *object;
-
-
-// for you to complete:
-object  make_object    (void *data, size_t fn_capacity);
-object  obj_define     (object obj, char *fn_name, void *(*fn_ptr)(void *));
-void *  obj_call       (object obj, char *fn);
